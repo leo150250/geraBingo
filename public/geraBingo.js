3ed3d91cb8999c52d5c70540ebf260ca75e37174
leo150250 <leo.producoes@hotmail.com>
2023-12-23 14:13:50 -0400

    first commit

---
+++ public/geraBingo.js
@@ -0,0 +1,71 @@
+var qtdNumeros=90;
+function gerarCartela() {
+    let divCartela=document.createElement("div");
+    divCartela.classList.add("cartela");
+    let numerosCartela=[];
+    let etapaLetra=0;
+    while (numerosCartela.length<25) {
+        //let novoNumero=numerosCartela.length+1;
+        var numeroConfirmado=false;
+        var novoNumero=0;
+        while (!numeroConfirmado) {
+            switch (etapaLetra) {
+                case 0: novoNumero=Math.floor(Math.random()*20)+1; break;
+                case 1: novoNumero=20+Math.floor(Math.random()*20)+1; break;
+                case 2: novoNumero=40+Math.floor(Math.random()*10)+1; break;
+                case 3: novoNumero=50+Math.floor(Math.random()*20)+1; break;
+                case 4: novoNumero=70+Math.floor(Math.random()*20)+1; break;
+            }
+            if (numerosCartela.indexOf(novoNumero,0)==-1) {
+                numeroConfirmado=true;
+            }
+        }
+        numerosCartela.push(novoNumero);
+        if (numerosCartela.length%5==0) {
+            etapaLetra++;
+        }
+    }
+    numerosCartela.sort(function (a,b) {
+        if (parseInt(a)>parseInt(b)) {
+            return 1;
+        }
+        if (parseInt(a)<parseInt(b)) {
+            return -1;
+        }
+        return 0;
+    })
+    for (let i=0; i<25; i++) {
+        if (i%5==0) {
+            let novaLetra="";
+            switch (i) {
+                case 0: novaLetra="B"; break;
+                case 5: novaLetra="I"; break;
+                case 10: novaLetra="N"; break;
+                case 15: novaLetra="G"; break;
+                case 20: novaLetra="O"; break;
+            }
+            divCartela.appendChild(gerarLetra(novaLetra));
+        }
+        var divNovoNumero=document.createElement("div");
+        if (i==12) {
+            divNovoNumero.innerHTML="&star;";
+            divNovoNumero.classList.add("estrela");
+        } else {
+            divNovoNumero.innerHTML=numerosCartela[i];
+        }
+        divCartela.appendChild(divNovoNumero);
+    }
+    document.body.appendChild(divCartela);
+    console.log("Pronto!");
+}
+function gerarCartelas() {
+    for (let i=0; i<100; i++) {
+        gerarCartela();
+    }
+}
+function gerarLetra(argLetra) {
+    var divNovaLetra=document.createElement("div");
+    divNovaLetra.classList.add("letra");
+    divNovaLetra.innerHTML=argLetra;
+    return divNovaLetra;
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Gerador de Cartelas de Bingo
+
+Este é um sistema que gera cartelas de bingo em HTML e JavaScript, que vão de 1 a 90, prontas para impressão.
+
+## Como usar
+
+Para usar o sistema, basta abrir o arquivo index.html em um navegador web. Não é necessário um servidor web.
+
+Você verá uma tela com dois botões, um para gerar uma cartela e outro para gerar 100 cartelas. Ao apertar neles, as cartelas serão geradas e mostradas na tela. A própria página você pode imprimir ou salvá-la como PDF para usar.
+
+## Tecnologias utilizadas
+
+O sistema foi desenvolvido usando HTML, CSS e JavaScript. Não há dependências externas. O código é simples, mas não foi comentado apenas para dificultar a sua compreensão sobre o mesmo.
+
+Quem sabe em 2024 eu comento esse negócio.
+
+Não me responsabilizo se sair cartela com número duplicado ou se seus níveis de ansiedade subirem enquanto sua família usa as cartelas geradas por esse sistema.
+
+## Licença
+
+Licença? Oi?
+
+Então... É quase MIT: Você pode usar, modificar, distribuir e, se quiser, dá os créditos pra mim.
